83c2e648e90840f80a706575d35faa7046099aed
Dan Comand <dcomand@gmail.com>
2015-04-21 22:24:35 -0400

    fixed an error with saving reviews.

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,16 +1,19 @@
 class ReviewsController < ApplicationController
-	  before_filter :ensure_logged_in, only: [:create, :destroy]
+
+  before_filter :ensure_logged_in, only: [:create, :destroy]
+  
   def show
   	@review = Review.find(params[:id])
   end
 
   def create
-  	@review = @product.reviews.build(review_params)
+  	@product = Product.find(params[:product_id])
+    @review = @product.reviews.build(review_params)
     @review.user = current_user
     if @review.save
-      redirect_to products_path, notice: 'Review created successfully'
+      redirect_to "/products/#{@product.id}", notice: "Review created successfully!"
     else
-      render 'products/show'
+      render 'product/show'
     end
   end
 
@@ -21,10 +24,7 @@ class ReviewsController < ApplicationController
 
   private
   def review_params
-    params.require(:review).permit(:comment, :product_id)
+    params.require(:review).permit(:comment, :product_id )
   end
 
-  def load_product
-    @product = Product.find(params[:product_id])
-  end
 end
