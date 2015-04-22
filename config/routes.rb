0e945ebadbe8b07d77a7345e2dc0f524689731fe
Dan Comand <dcomand@gmail.com>
2015-04-21 22:15:13 -0400

    adding user names.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
 
-resources :products do
-resources :reviews, only: [:show, :create, :destroy]
-resources :sessions, only: [:new, :create, :destroy]
-resources :users, only: [:new, :create]
+  root 'products#index'
 
+  resources :products
+
+  resources :users, only: [:new, :create]
+
+  resources :sessions, only: [:new, :create, :destroy]
+
+  resources :products do
+    resources :reviews, only: [:show, :create, :destroy]
+  end
 end
