dbc8d5d2ad9965d95b1e78ac450184cc9c4fbef2
Dan Comand <dcomand@gmail.com>
2015-05-05 16:58:35 -0400

    added jquery to products.js

---
+++ app/assets/javascripts/products.js
@@ -0,0 +1,18 @@
+$('document').on('ready page:load', function() {
+$('search-form').submit(function(event) {
+	event.prevent.Default();
+	var searchValue = $('#search').val();
+
+	$.ajax({
+		url: '/products?search=' + searchValue,
+		type: 'GET',
+		dataType: 'html'
+	}).done(function(data){
+		$('#products').html(data);
+
+
+	});
+
+	});
+
+});
