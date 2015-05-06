1c467e7b3b128602a4713e47aaec3868407183d2
Dan Comand <dcomand@gmail.com>
2015-05-06 16:49:37 -0400

    infinite scrolling is working.

--- app/assets/javascripts/products.js
+++ app/assets/javascripts/products.js
@@ -1,14 +1,11 @@
-$('document').on('ready page:load', function() {
-$('search-form').submit(function(event) {
-	event.prevent.Default();
-	var searchValue = $('#search').val();
-
-	   $.getScript('/products?search=' + searchValue);
-      .done(function(data){
-        console.log(data);
-        $('#products').html(data);
-	});
-
-	});
-
+$(document).on('ready page:load', function() {
+  if ($('.pagination').length) {
+    $(window).scroll(function() {
+      var url = $('.pagination span.next').children().attr('href');
+      if (url && $(window).scrollTop() > $(document).height() - $(window).height() - 50) {
+        $('.pagination').text("Fetching more products...");
+        return $.getScript(url);
+      }
+    });
+  }
 });
